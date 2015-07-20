778f47a9a9e81ff909683787e9692d01c94a8a06
Michel Corne <miccorne@gmail.com>
2015-07-20 16:30:02 +0200

    fixing types

---
+++ ada2php/php/enumeration.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'type.php';
+
+class enumeration extends type
+{
+    public $values;
+    public $indexes;
+
+    public function set_value($value)
+    {
+        $this->data = $value;
+    }
+
+    /**
+     *
+     * @param mixed $value
+     * @throws Exception
+     */
+    public function validate($value)
+    {
+        if (! is_null($value) and (! is_string($value) or ! isset($this->indexes[$value]))) {
+            throw new Exception('Invalid enumeration value');
+        }
+    }
+
+    /**
+     *
+     * @param array $values
+     * @throws Exception
+     */
+    public function validate_type_properties($values = null)
+    {
+        foreach($values as $index => $value) {
+            if (! is_string($value)) {
+                throw new Exception('Non string enumeration value');
+            }
+
+            if (isset($this->indexes[$value])) {
+                throw new Exception("Enumeration value already exists: $value");
+            }
+
+            $this->indexes[$value] = $index;
+        }
+    }
+}
