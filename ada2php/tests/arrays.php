761fe84bc5a84fae2bb05a30a2e46423872c3e53
Michel Corne <miccorne@gmail.com>
2015-07-24 17:25:23 +0200

    fixing types

--- ada2php/tests/arrays.php
+++ ada2php/tests/arrays.php
@@ -8,7 +8,11 @@ arrays::new_type('new_array', 'integer', 'integer');
 arrays::new_type('new_array', 'integer', [0, 5]);
 arrays::new_type('new_array', 'integer', ['integer', 1, 5]);
 
-arrays::create([1, 2, 3, 4, 5]); // to be mapped to the keys
+$a = arrays::create();
+
+$a = arrays::create([1, 2, 3, 4, 5]); // to be mapped to the keys
+$a = arrays::create([0 => 1, 1 => 2, 2 => 3, 3 => 4, 4 => 5], false); // keys and values
+
 
 arrays::new_type('new_array', 'integer', 'char');
 arrays::new_type('new_array', 'integer', ['char', 'a', 'f']);
